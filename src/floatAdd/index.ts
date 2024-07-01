6d2362b5e54154190980813985605fc812566de0
熊珂 <xiongke@xiongkedeMacBook-Pro.local>
2024-07-01 17:25:50 +0800

    fix: add multiple parameter

--- src/floatAdd/index.ts
+++ src/floatAdd/index.ts
@@ -1,13 +1,11 @@
-export function floatAdd(num1: number, num2: number): number {
-    if(num1===undefined){
-        num1=0
-    }
-    if(num2===undefined){
-       num2 = 0
-    }
-    const maxDecimals = Math.max(countDecimals(num1), countDecimals(num2))
+export function floatAdd(...nums:number[]): number {
+    if(nums.length === 0) return 0
+    const maxDecimals = Math.max(...nums)
     const precision = 1*Math.pow(10,maxDecimals)
-    return ((num1*precision)+(num2*precision))/precision
+    return nums.reduce((pre,cur)=>{
+        if(cur===undefined) cur = 0
+        return ((pre*precision)+(cur*precision))/precision
+     },0)
  }
 
  function countDecimals(num:number){
