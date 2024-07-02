5f04d87ff2b11427a300ebbb0f0e02273a2db812
陈涛 <jooies@qq.com>
2024-07-02 15:48:08 +0800

    fix: eslint bug

---
+++ .eslintrc.cjs
@@ -0,0 +1,30 @@
+/* eslint-env node */
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+    parser: '@typescript-eslint/parser',
+    plugins: ['@typescript-eslint'],
+    root: true,
+    rules: {
+        'quotes': [1, 'single'],
+        'semi': [1, 'never'],
+        'keyword-spacing': ['error', { before: true, after: true }],
+        'arrow-spacing': ['error', { before: true, after: true }],
+        'space-before-blocks': 'error',
+        'no-unused-vars': 0,
+        'space-infix-ops': 2,
+        'no-var': 0,
+        'prefer-const': 2,
+        'object-shorthand': 2,
+        'no-trailing-spaces': 2,
+        'no-undef': 0,
+        'space-unary-ops': 1,
+        'no-irregular-whitespace': 1,
+        'no-multi-spaces': 1,
+        'comma-spacing': [1, { 'before': false, 'after': true }],
+        'no-whitespace-before-property': 1,
+        'no-empty': 0,
+        'no-redeclare': 0,
+        'no-inner-declarations': 0,
+        '@typescript-eslint/no-explicit-any': 0,
+    }
+}
