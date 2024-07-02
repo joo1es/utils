33bba05ebc31754550e2f52e491c5ccd1f0bedee
陈涛 <jooies@qq.com>
2024-07-02 15:40:09 +0800

    feat: add lint

--- src/floatAdd/index.test.ts
+++ src/floatAdd/index.test.ts
@@ -1,41 +1,41 @@
-import { floatAdd, countDecimals } from "."
+import { floatAdd, countDecimals } from '.'
 
 it('should add two positive numbers correctly', () => {
-    expect(floatAdd(0.1, 0.2)).toBe(0.3);
-    expect(floatAdd(0.01, 0.02)).toBe(0.03);
-    expect(floatAdd(1, 0.2)).toBe(1.2);
-    expect(floatAdd(1, 1)).toBe(2);
+    expect(floatAdd(0.1, 0.2)).toBe(0.3)
+    expect(floatAdd(0.01, 0.02)).toBe(0.03)
+    expect(floatAdd(1, 0.2)).toBe(1.2)
+    expect(floatAdd(1, 1)).toBe(2)
 
-    expect(floatAdd(0.1, 0.2, 0.3)).toBe(0.6);
-    expect(floatAdd(0.1, 0.2, 0.3, 0.4)).toBe(1);
+    expect(floatAdd(0.1, 0.2, 0.3)).toBe(0.6)
+    expect(floatAdd(0.1, 0.2, 0.3, 0.4)).toBe(1)
 })
 it('should add two negative numbers correctly', () => {
-    expect(floatAdd(-0.1, -0.2)).toBe(-0.3);
-    expect(floatAdd(-0.1, -0.2, -0.3)).toBe(-0.6);
+    expect(floatAdd(-0.1, -0.2)).toBe(-0.3)
+    expect(floatAdd(-0.1, -0.2, -0.3)).toBe(-0.6)
 })
 it('should add a positive and a negative number correctly', () => {
-    expect(floatAdd(1.5, -0.5)).toBe(1);
-    expect(floatAdd(1.5, -0.5, -0.5)).toBe(0.5);
-});
+    expect(floatAdd(1.5, -0.5)).toBe(1)
+    expect(floatAdd(1.5, -0.5, -0.5)).toBe(0.5)
+})
 it('should add a zero and a zero number correctly', () => {
-    expect(floatAdd(0, 0)).toBe(0);
-    expect(floatAdd(0, 0, 0)).toBe(0);
-});
-it('should add a zero and a positive number correctly', () => {
-    expect(floatAdd(0, 1)).toBe(1);
-    expect(floatAdd(0, 1, 0)).toBe(1);
+    expect(floatAdd(0, 0)).toBe(0)
+    expect(floatAdd(0, 0, 0)).toBe(0)
 })
 it('should add a zero and a positive number correctly', () => {
-    expect(floatAdd(0, 0.1)).toBe(0.1);
-    expect(floatAdd(0, 0.1, 0)).toBe(0.1);
+    expect(floatAdd(0, 1)).toBe(1)
+    expect(floatAdd(0, 1, 0)).toBe(1)
+})
+it('should add a zero and a positive number correctly', () => {
+    expect(floatAdd(0, 0.1)).toBe(0.1)
+    expect(floatAdd(0, 0.1, 0)).toBe(0.1)
 })
 it('should add a zero and a negative number correctly', () => {
-    expect(floatAdd(0, -0.1)).toBe(-0.1);
-    expect(floatAdd(0, -0.1, 0)).toBe(-0.1);
+    expect(floatAdd(0, -0.1)).toBe(-0.1)
+    expect(floatAdd(0, -0.1, 0)).toBe(-0.1)
 })
 it('should be a number', () => {
-    expect(floatAdd(0, NaN, Math.log(0))).toBe(0);
-    expect(floatAdd(Math.pow(10, 1000), Math.log(0), -Math.pow(10, 1000))).toBe(0);
+    expect(floatAdd(0, NaN, Math.log(0))).toBe(0)
+    expect(floatAdd(Math.pow(10, 1000), Math.log(0), -Math.pow(10, 1000))).toBe(0)
 })
 it('test countDecimals invaild', () => {
     expect(countDecimals(0.001)).toBe(3)
